7f38d7fc310ae08173c28245ffa5885a8cf52123
Rob Cameron <rob.cameron@fastmail.com>
2020-02-20 10:54:57 -0800

    Responsive styles for index, show/hide nav

--- code/javascripts/controllers/application_controller.js
+++ code/javascripts/controllers/application_controller.js
@@ -2,10 +2,15 @@ import { Controller } from "stimulus";
 
 export default class extends Controller {
   static get targets() {
-    return ["year"];
+    return ["nav", "year"];
   }
 
   connect() {
     this.yearTarget.textContent = new Date().getFullYear();
+  }
+
+  toggleNav() {
+    this.navTarget.classList.toggle("hidden");
+    document.body.classList.toggle("fixed");
   }
 }
