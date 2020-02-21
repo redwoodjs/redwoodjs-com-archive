fa1e8b817ff04e9fecb529ff33a6cc2e331d0e9c
Rob Cameron <rob.cameron@fastmail.com>
2020-02-20 18:09:49 -0800

    Whitelist another class

--- postcss.config.js
+++ postcss.config.js
@@ -8,7 +8,7 @@ module.exports = {
       require("@fullhuman/postcss-purgecss")({
         content: ["./publish/**/*.html"],
         defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || [],
-        whitelist: ["tab-active", "searchresult", "w-1/3", "w-2/3"]
+        whitelist: ["tab-active", "searchresult", "w-1/3", "w-2/3", "hover:bg-red-100"]
       })
   ]
 };
