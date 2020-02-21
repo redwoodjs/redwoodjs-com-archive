5fbae3ed75cb34fd915f848c8121375cda043fd3
Rob Cameron <rob.cameron@fastmail.com>
2020-02-20 18:07:46 -0800

    Whitelist a couple CSS classes

--- postcss.config.js
+++ postcss.config.js
@@ -8,7 +8,7 @@ module.exports = {
       require("@fullhuman/postcss-purgecss")({
         content: ["./publish/**/*.html"],
         defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || [],
-        whitelist: ["tab-active"]
+        whitelist: ["tab-active", "searchresult", "w-1/3", "w-2/3"]
       })
   ]
 };
