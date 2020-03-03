b1441f106e1b8d3a245730939b51e85e32e12c36
Rob Cameron <rob.cameron@fastmail.com>
2020-03-03 09:26:39 -0800

    Adds copy to code blocks

--- code/javascripts/controllers/application_controller.js
+++ code/javascripts/controllers/application_controller.js
@@ -7,13 +7,21 @@ export default class extends Controller {
   }
 
   connect() {
+    // set the year in the footer
     this.yearTarget.textContent = new Date().getFullYear();
+
+    // code highlighting
     this.codeTargets.forEach(target => {
       hljs.highlightBlock(target);
     });
+
+    // show the header logo unless we're on the homepage
     if (!this.isHomePage) {
       this.logoTarget.classList.remove("lg:hidden");
     }
+
+    // add copy buttons to code blocks
+    this._enableCopy();
   }
 
   focusSearch(event) {
@@ -25,6 +33,46 @@ export default class extends Controller {
   toggleNav() {
     this.navTarget.classList.toggle("hidden");
     document.body.classList.toggle("fixed");
+  }
+
+  _enableCopy() {
+    const COPY_BUTTON_CSS = [
+      "copy-button",
+      "absolute",
+      "right-0",
+      "bottom-0",
+      "m-2",
+      "text-xs",
+      "text-gray-500",
+      "hover:text-gray-400",
+      "bg-gray-800",
+      "hover:bg-gray-700",
+      "px-1",
+      "rounded",
+      "focus:outline-none",
+      "transition",
+      "duration-100"
+    ];
+    const codeBlocks = document.getElementsByTagName("code");
+    for (let block of codeBlocks) {
+      const parent = block.parentElement;
+
+      // is this is a copyable code block <pre><code>...</code></pre>
+      if (parent.tagName === "PRE") {
+        parent.classList.add("relative");
+        var button = document.createElement("button");
+        button.classList.add(...COPY_BUTTON_CSS);
+        button.textContent = "Copy";
+        block.parentElement.appendChild(button);
+
+        new ClipboardJS(".copy-button", {
+          target: function(trigger) {
+            return trigger.previousElementSibling;
+          }
+        });
+      }
+    }
+    // new ClipboardJS("code");
   }
 
   get isHomePage() {
