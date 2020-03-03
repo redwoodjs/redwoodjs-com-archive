0076caefce1d8a741bf7e3ed5a7b8a5479965d75
Rob Cameron <rob.cameron@fastmail.com>
2020-03-03 09:28:41 -0800

    Whitelist classes set in JS

--- postcss.config.js
+++ postcss.config.js
@@ -15,7 +15,21 @@ module.exports = {
           "md:w-1/3",
           "md:w-2/3",
           "border-b",
-          "hover:bg-red-100"
+          "hover:bg-red-100",
+          "absolute",
+          "right-0",
+          "bottom-0",
+          "m-2",
+          "text-xs",
+          "text-gray-500",
+          "hover:text-gray-400",
+          "bg-gray-800",
+          "hover:bg-gray-700",
+          "px-1",
+          "rounded",
+          "focus:outline-none",
+          "transition",
+          "duration-100"
         ]
       })
   ]
